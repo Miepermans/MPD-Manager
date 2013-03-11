062fd736d9f88b1e52e698eb8bba22ff3df3b59d
Miepermans <miep@pixelbar.nl>
2013-03-11 18:20:23 +0100

    Added Twitterbootstrap, and other functionality

---
+++ frontend/index.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once "head.html";
+include "../include/dbinfo.inc.php";
+include "../include/config.inc.php";
+include "../include/mpd.class.php";
+
+echo '
+
+
+    <div class="container">
+
+
+    </div> <!-- /container -->
+';
+ 
+ include_once "footer.html";
+ ?>
